1c71eb46ab2f8e71ddd6e8b682447b953b688dfd
Safiqul Islam <safiqulislam@safiqul.local>
2017-03-26 01:30:38 +0530

    sari's change viewcolor

--- swifty/ViewController.swift
+++ swifty/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor .blackColor()
+        self.view.backgroundColor = UIColor .redColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
